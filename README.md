f8b59251f816906bf10db8e30d8480851c59b1cf
David Bradway <dpb6@duke.edu>
2016-09-29 14:48:25 -0400

    changed arguments! auto-detect parallel pool!

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # smartParFor
 
-this is a small Git repo demonstrating a structure for a 'smart' parfor loop that defaults back to a 'for' loop with a flag input
+this is a small Git repo demonstrating a structure for a 'smart' parfor loop that defaults back to a 'for' loop when there is no parallel pool open
--- smartParFor.m
+++ smartParFor.m
@@ -1,23 +1,45 @@
-function g = smartParFor(parallel,handle,startIndex,stopIndex)
-% This is a wrapper function that allows us to call a function
-% snippet in either a 'parfor' or 'for' loop structure.
-% This keeps us from having to repeat ourselves.
-% Use a function handle before this function call:
+function g = smartParFor(handle,varargin)
+% SMARTPARFOR call a function in either a 'parfor' or 'for' loop
+%   G = SMARTPARFOR(HANDLE,VECTOR) run function for each element of vector
+%   G = SMARTPARFOR(HANDLE,START,STOP) run function for [start:stop]
+%   G = SMARTPARFOR(HANDLE,START,STOP,STEP) run function for [start:step:stop]
 %
-% g(5)=0;
-% parallel=0;
+% startIndex,stopIndex
+%
 % handle = @loopContents;
 % startIndex = 1;
 % stopIndex = 5;
-% g=smartParFor(parallel,handle,startIndex,stopIndex)
+% g(5)=0;
+% g=smartParFor(handle,startIndex,stopIndex)
+% gcp
+% g2(5)=0;
+% g2=smartParFor(handle,startIndex,stopIndex)
+% delete(gcp('nocreate'))
 
-g=nan(1,length(startIndex:stopIndex));
-if parallel
-    parfor i=startIndex:stopIndex
-        g(i)=feval(handle,i);
+switch nargin
+    case 2
+        vector = varargin{1};
+    case 3
+        vector = [varargin{1}:varargin{2}];
+    case 4
+        vector = [varargin{1}:varargin{3}:varargin{2}];
+    otherwise
+        error('Expected 2, ,3 or 4 arguments')
+end
+
+g=nan(1,length(vector));
+
+% so we start by checking whether the pool is open. 
+p = gcp('nocreate');
+
+if isempty(p)
+    % If there is no pool, run as sequential for loop
+    for i=vector
+        g(i)=handle(i);
     end
 else
-    for i=startIndex:stopIndex
-        g(i)=handle(i);
+    % If there is a pool, run as a parfor loop
+    parfor i=vector
+        g(i)=feval(handle,i);
     end
 end
